ae76e13b69bb69f2e7308cdae043c20b72e69087
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-10-26 20:12:21 -0600

    Move most of runner into Battleship, created instance variables in battleship

--- runner.rb
+++ runner.rb
@@ -4,11 +4,5 @@ require './lib/ship'
 require './lib/cell'
 require './lib/player'
 require './lib/computer'
-
 battleship = Battleship.new
-player_board = Board.new
-computer_board = Board.new
-computer = Computer.new(computer_board)
-player = Player.new(player_board)
-
-battleship.greeting(computer, player)
+battleship.greeting
